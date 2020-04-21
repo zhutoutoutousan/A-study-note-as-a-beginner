bb92ddc684783c984b5b7805607aa76aa2b7de70
zhutoutoutousan <31960995+zhutoutoutousan@users.noreply.github.com>
2020-04-21 11:25:15 +0800

    123
    123
    
    
    Former-commit-id: 296c25cb39e1d847d4e2f5c5922b82fd65072157 [formerly 6fd94035ac85a09b8c1b5904b4e25aff96ab22cc]
    Former-commit-id: 3352cfeee4fa05e6af982cb81b312f0c9d962dc7

--- src/components/Button/AnimationButton.vue
+++ src/components/Button/AnimationButton.vue
@@ -1,18 +1,20 @@
 <template>
-    <div class="ripple">
-        <button @click="toggleRipple()">Click me to Toggle</button>
+    <div class="div">
+        <button class="ripple" @click="toggleRipple()">Click me to Toggle</button>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
+    name: "AnimationButton",
     mounted(){
         // Do something
     },
     methods: {
         crossActivate(id){
-            // Do something
+            const a = id;
+            return a;
         }
     }
 })
@@ -24,5 +26,25 @@ export default Vue.extend({
     &:hover {
         background: #3086cc radial-gradient(circle, transparent 2%, #47a7f3 3%) center/15000%;
     }
+    &:active {
+        background-color: #7f15a8;
+        background-size: 100%;
+        transition: background 0s;
+    }
 }
+
+button {
+    border: none;
+    border-radius: 2px;
+    padding: 12px 18px;
+    font-size: 16px;
+    text-transform: uppercase;
+    cursor: pointer;
+    color: white;
+    background-color: #21976f;
+    box-shadow: 0 0 4px #999;
+    outline: none;
+}
+
+
 </style>

4666ea5b97729ee0d35c51f3c15da113e05ced4e
zhutoutoutousan <31960995+zhutoutoutousan@users.noreply.github.com>
2020-04-30 17:48:59 +0800

    1
    1
    
    
    Former-commit-id: ddc671ff04dc90f7fe93e7660bac4597d69ee733 [formerly 83407643b9950c7dc6231bc6cff992f58f73f20e]
    Former-commit-id: 049a3de843fe5416120e5778df26966d7a2db169

--- crawler/server.js
+++ crawler/server.js
@@ -1,24 +1,27 @@
 // https://scotch.io/tutorials/scraping-the-web-with-node-js
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+console.log('Processing code');
 
-let express = require('express');
-let fs = require('fs');
-let request = require('request');
-let cheerio = require('cheerio');
-let app     = express();
+app.get('/', function(req, res){
 
-app.get('/scrape', function(req, res){
+var url = 'https://www.imdb.com/title/tt1229340/';
 
-let url = 'http://www.imdb.com/title/tt1229340/';
+console.log('Processing code');
 
 request(url, function(error, response, html){
+    console.log('Processing request');
     if(!error){
-        let $ = cheerio.load(html);
+    var $ = cheerio.load(html);
 
-    let title, release, rating;
-    let json = { title : "", release : "", rating : ""};
+    var title, release, rating;
+    var json = { title : "", release : "", rating : ""};
 
     $('.header').filter(function(){
-        let data = $(this);
+        var data = $(this);
         title = data.children().first().text();            
         release = data.children().last().children().text();
 
@@ -27,29 +30,27 @@ request(url, function(error, response, html){
     })
 
     $('.star-box-giga-star').filter(function(){
-        let data = $(this);
+        var data = $(this);
         rating = data.text();
 
         json.rating = rating;
     })
 }
 
-// To write to the system we will use the built in 'fs' library.
-// In this example we will pass 3 parameters to the writeFile function
-// Parameter 1 :  output.json - this is what the created filename will be called
-// Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
-// Parameter 3 :  callback function - a callback function to let us know the status of our function
+app.listen('8081', () => console.log('The server is listening at port 8081'))
+
+exports = module.exports = app;
 
 fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-
+    if(!err){
     console.log('File successfully written! - Check your project directory for the output.json file');
-
+    }
+    else{
+        console.log('Something went wrong.')
+    }
 })
 
-// Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
 res.send('Check your console!')
-
     }) ;
 })
 
-exports = module.exports = app;

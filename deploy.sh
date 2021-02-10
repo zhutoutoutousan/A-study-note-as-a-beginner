da41a8ea5ded4fb64eca9ec7e5e1effc4f5b9d03
zhutoutoutousan <mxyuiiu64545@126.com>
2021-02-10 20:48:49 +0800

    Aktualisierung

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
+echo Plz, your username
+read username
+echo Upload to $username
+
 git add .
 git commit -am "Aktualisierung"
-git push git@github.com:zhutoutoutousan/A-study-note-as-a-beginner.git
+git push git@github.com:$username/A-study-note-as-a-beginner.git

11ffaef4f1a68fad4d3b16d8a64c6ec053700626
zhutoutoutousan <mxyuiiu64545@126.com>
2021-02-16 18:12:13 +0800

    Aktualisierung

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,17 @@
-echo Plz, your username
-read username
-echo Upload to $username
+echo "Plz, your username(Default: zttts)"
+read tmp
+if [ -z "$tmp"]
+then
+    echo "Using default name"
+    username="zhutoutoutousan"
+else
+    echo "Using $tmp"
+    username=$tmp
+fi
+
+unset tmp
+
+echo Uploading to $username
 
 git add .
 git commit -am "Aktualisierung"

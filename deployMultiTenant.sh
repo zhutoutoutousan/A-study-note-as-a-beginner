f450badc96404612f90948e9bdae4f94db59edd0
zhutoutoutousan <mxyuiiu64545@126.com>
2022-03-01 11:40:42 +0800

    Aktualisierung 2022-03-01-11-40-41

--- deployMultiTenant.sh
+++ deployMultiTenant.sh
@@ -1,4 +1,4 @@
-echo "Plz, your username(Default: zttts)"
+echo "Plz, your username(Default: Current Git Username)"
 read tmp
 if [ -z "$tmp"]
 then
@@ -17,15 +17,28 @@ read tmp
 if [ -z "$tmp"]
 then
     echo "Using default message"
-    commitMessage="Aktualisierung"
+    # Append current time to commit message
+    commitMessage="Aktualisierung $(date +%Y-%m-%d-%H-%M-%S)"
 else
     echo "Using $tmp"
     commitMessage=$tmp
 fi
 
-
+# get the current git repository name and save it to a variable
+repoUrl=$(git config remote.origin.url | sed 's/.*:\/\/github.com\///' | sed 's/\.git//')
+repoName=$(echo $repoUrl | sed 's/.*\///')
 echo Uploading to $username
 
 git add .
 git commit -am "$commitMessage"
-git push git@github.com:$username/A-study-note-as-a-beginner.git
+
+# if $username is not empty, then upload to github
+if [ -n "$username" ]
+then
+    git push git@github.com:$username/A-study-note-as-a-beginner.git
+else
+    # Use $repoUrl
+    echo "Using current repoUrl: $repoUrl"
+    git push $repoUrl
+fi
+
